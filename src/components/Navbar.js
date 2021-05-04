0c3f6b31d2b522687e8ee9300b5c621393ba5bc6
AnubisF <artem.vinogradov@hotmail.com>
2021-05-04 14:06:29 +0300

    Codding ;)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
 import React, {useState} from "react";
+import { Button } from './Button';
 import { Link }  from 'react-router-dom';
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu =() => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+        setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    window.addEventListener('resize', showButton);
+
     return (
         <>
             <nav className="navbar">
@@ -38,10 +52,11 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle='btn--outline'>SING UP</Button>}
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
 export default Navbar
