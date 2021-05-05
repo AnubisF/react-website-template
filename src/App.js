9d6a5288857a3c8e4e05d7f4d834c24effde603c
AnubisF <artem.vinogradov@hotmail.com>
2021-05-05 09:55:28 +0300

    Added some files, some change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import { BrowserRouter  as Router, Switch, Route} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from './components/pages/Home';
+import About from "./components/pages/About";
+import Skills from "./components/pages/Skills";
+import Projects from "./components/pages/Projects";
+import SignUp from "./components/pages/SIgnUp";
 
 function App() {
   return (
@@ -13,6 +17,7 @@ function App() {
                 <Route path='/' exact components={Home} />
                 <Route path='/about' component={About} />
                 <Route path='/skills' component={Skills} />
+                <Route path='/projects' component={Projects} />
                 <Route path='/sign-up' component={SignUp} />
             </Switch>
         </Router>
